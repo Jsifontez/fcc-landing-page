8981fde03c1fc05728d36eb22e695239b785ffb1
Juan Sifontez <juansfontz@gmail.com>
2020-05-12 20:07:24 -0400

    Add swipe events

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ const $menuButton = document.getElementById('icon-menu')
 $menuButton.addEventListener('click', displayMenu)
 
 function displayMenu (ev) {
-  console.log($header.classList)
   $header.classList.toggle('active')
   $menu.classList.toggle('active')
 }
+
+function showMenu (ev) {
+  $header.classList.add('active')
+  $menu.classList.add('active')
+}
+
+function hideMenu (ev) {
+  $header.classList.remove('active')
+  $menu.classList.remove('active')
+}
+
+const $body = document.body
+var gestos = new Hammer($body);
+gestos.on('swipeleft', showMenu);
+gestos.on('swiperight', hideMenu);
